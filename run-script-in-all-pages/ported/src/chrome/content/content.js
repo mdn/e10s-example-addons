a23d28a8f2e48d9c45a81c30c685bf17357a4ab1
Will Bamberg <wbamberg@mozilla.com>
2014-06-22 17:20:46 -0700

    remove the old version of 'ported'

--- run-script-in-all-pages/ported/src/chrome/content/content.js
+++
@@ -1,6 +0,0 @@
-addEventListener("DOMContentLoaded", function(event) {
-  var doc = event.originalTarget;
-  if (doc.nodeName != "#document") return; // only documents 
-  doc.body.style.border = "5px solid red";
-});
-
--- run-script-in-all-pages/ported/src/chrome/content/modify-all-pages.js
+++
@@ -1,5 +0,0 @@
-// chrome script
-let globalMM = Cc["@mozilla.org/globalmessagemanager;1"]
-  .getService(Ci.nsIMessageListenerManager);
-
-globalMM.loadFrameScript("chrome://modify-all-pages/content/content.js", true);

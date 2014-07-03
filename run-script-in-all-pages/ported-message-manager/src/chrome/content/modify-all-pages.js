53412443c09faa130e50ff9e9bafb4377cde2201
Will Bamberg <wbamberg@mozilla.com>
2014-07-02 21:11:08 -0700

    minor code cleanup

--- run-script-in-all-pages/ported-message-manager/src/chrome/content/modify-all-pages.js
+++ run-script-in-all-pages/ported-message-manager/src/chrome/content/modify-all-pages.js
@@ -1,5 +1,5 @@
 // chrome script
-let globalMM = Cc["@mozilla.org/globalmessagemanager;1"]
+var globalMM = Cc["@mozilla.org/globalmessagemanager;1"]
   .getService(Ci.nsIMessageListenerManager);
 
-globalMM.loadFrameScript("chrome://modify-all-pages/content/content.js", true);
+globalMM.loadFrameScript("chrome://modify-all-pages/content/frame-script.js", true);
